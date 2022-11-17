108e3b30a1be4dba403dad7e2524bf1a290d2b51
test <test>
2022-11-17 12:24:22 +0500

    js2

---
+++ script.js
@@ -0,0 +1,41 @@
+var modal = prompt("Ismingizni kiriting")
+console.log("Sizning ismingiz "+modal)
+
+var modal = +prompt("Yoshingizni kiriting")
+console.log("Sizning yoshingiz "+modal +" da")
+
+var misol = +prompt("22+33=?")
+if (misol == 55){
+    console.log("Sizning javobingiz "+misol +" to'g'ri")
+}else {
+    console.log("Sizning javobingiz: " +misol +" noto'g'ri" +". To'g'ri javob 55")
+}
+
+var misol = +prompt("119-34=?")
+if (misol == 85){
+    console.log("Sizning javobingiz " +misol +" to'g'ri")
+}else {
+    console.log("Sizning javobingiz: " +misol  +" noto'g'ri" +". To'g'ri javob 85")
+}
+
+var misol = +prompt("35*8=?")
+if (misol == 280){
+    console.log("Sizning javobingiz " +misol +" to'g'ri")
+}else {
+    console.log("Sizning javobingiz: " +misol  +" noto'g'ri" +". To'g'ri javob 280")
+}
+
+var misol = +prompt("140:20=?")
+if (misol == 7){
+    console.log("Sizning javobingiz " +misol +" to'g'ri")
+}else {
+    console.log("Sizning javobingiz: " +misol  +" noto'g'ri" +". To'g'ri javob 7")
+}
+
+var misol = +prompt("39%2=?")
+if (misol == 1){
+    console.log("Sizning javobingiz "+misol +" to'g'ri")
+}else {
+    console.log("Sizning javobingiz: " +misol  +" noto'g'ri" +". To'g'ri javob 1")
+}
+alert("Javoblarni tekshirish uchun konsolni oching")
